449bcab2c5087122ba6266ed666e537f670853a5
Kaio-rocha-sptech <kaio.rocha@@sptech.school>
2025-12-11 10:59:07 -0300

    Tirando aquarios

--- src/routes/salas.js
+++ src/routes/salas.js
@@ -1,14 +1,14 @@
 var express = require("express");
 var router = express.Router();
 
-var aquarioController = require("../controllers/aquarioController");
+var salaController = require("../controllers/salaController");
 
 router.get("/:empresaId", function (req, res) {
-  aquarioController.buscarAquariosPorEmpresa(req, res);
+  salaController.buscarSalasPorEmpresa(req, res);
 });
 
 router.post("/cadastrar", function (req, res) {
-  aquarioController.cadastrar(req, res);
+  salaController.cadastrar(req, res);
 })
 
 module.exports = router;
